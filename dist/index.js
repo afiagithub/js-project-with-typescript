a6ae6c91975dc6b6f79f4b010d0224e88854d0d0
Afia Rahman <rahmanmou26@gmail.com>
2024-08-23 16:35:37 +0600

    created class with typescript keywords

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,22 @@
 "use strict";
 console.log('hello, afia');
 console.log('new typescript project');
+// class User {
+//     email: string
+//     name: string
+//     readonly city: string = 'dhaka' //you have to initialize it at some point otherwise error
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+//     }
+// }
+class User {
+    constructor(email, name, userId) {
+        this.email = email;
+        this.name = name;
+        this.userId = userId;
+        this.city = 'dhaka'; //you have to initialize it at some point otherwise error
+    }
+}
+let afia = new User('afia@rahman.com', 'afia rahman', 549);
+console.log(afia.city);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
 console.log('hello, afia');
 console.log('new typescript project');
+
+// class User {
+//     email: string
+//     name: string
+//     readonly city: string = 'dhaka' //you have to initialize it at some point otherwise error
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+//     }
+// }
+
+class User {
+    readonly city: string = 'dhaka' //you have to initialize it at some point otherwise error
+    constructor(public email: string,
+        public name: string,
+        private userId: number) {
+    }
+}
+
+let afia = new User('afia@rahman.com', 'afia rahman', 549)
+console.log(afia.city);
